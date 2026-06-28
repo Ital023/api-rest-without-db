db5cd60a4c2c55d4710222a8d537b47a7f014e72
Italo Miranda <contadoitalo911@gmail.com>
2026-06-28 14:47:26 -0300

    Add API handler and server setup with chi middleware
    - Implemented NewHandler function for routing
    - Added POST and GET endpoints for URL shortening
    - Updated main function to start the HTTP server
    - Added go-chi dependency in go.mod

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+	"net/http"
+	"os"
+	"projeto_1_sem_db/api"
+	"time"
+)
 
 func main() {
 	if err := run(); err != nil {
 		slog.Error("failed to execute code", "error", err)
-		return
+		os.Exit(1)
 	}
 	slog.Info("all systems offline")
 }
 
 func run() error {
+	handler := api.NewHandler()
+	s := http.Server{
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      handler,
+	}
 
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
 }
